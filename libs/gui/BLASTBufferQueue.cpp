c010f956d96d8924359b81537daac45038ff6bfa
hirero765 <forpayment169@gmail.com>
2023-03-04 21:28:08 +0530

    BLASTBufferQueue: Comment out lines causing log spam * Reference: https://stackoverflow.com/questions/74722563/how-do-i-trace-and-resolve-android-apps-blastbufferqueue-faking-releasebufferca

--- libs/gui/BLASTBufferQueue.cpp
+++ libs/gui/BLASTBufferQueue.cpp
@@ -357,8 +357,8 @@ void BLASTBufferQueue::transactionCallback(nsecs_t /*latchTime*/, const sp<Fence
                     }
                 }
                 for (const auto& staleRelease : staleReleases) {
-                    BQA_LOGV("Faking releaseBufferCallback from transactionCompleteCallback");
-                    BBQ_TRACE("FakeReleaseCallback");
+                   // BQA_LOGV("Faking releaseBufferCallback from transactionCompleteCallback");
+                   // BBQ_TRACE("FakeReleaseCallback");
                     releaseBufferCallbackLocked(staleRelease,
                         stat.previousReleaseFence ? stat.previousReleaseFence : Fence::NO_FENCE,
                         stat.currentMaxAcquiredBufferCount);
